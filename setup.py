bcefddd0d65bf1775f9b45a7f47469285e1504fc
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-07-14 22:50:12 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name = 'py-oeis',
+    version = '1.0',
+    description = description,
+    author = 'Sumant Bhaskaruni',
+    author_email = 'bsumantb@gmail.com',
+    license = 'MIT',
+    url = 'https://github.com/totallyhuman/py-oeis',
+    py_modules = ['oeis'],
+    install_requires = ['pendulum', 'requests']
+)
